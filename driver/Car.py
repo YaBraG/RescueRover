83efeaa5f4ed902df2cb543c18fc7f5c4ab5cd76
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-11-06 17:59:57 -0500

    Update Car.py

--- driver/Car.py
+++ driver/Car.py
@@ -2,6 +2,7 @@ import socketio
 import time
 import RPi.GPIO as GPIO
 
+
 def remap(x, oMin, oMax, nMin, nMax):
 
     # range check
@@ -37,29 +38,35 @@ def remap(x, oMin, oMax, nMin, nMax):
 
     return result
 
+
 sio = socketio.Client()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
 
 class Motor():
     def __init__(self, Ena, In1, In2):
         self.Ena = Ena
         self.In1 = In1
         self.In2 = In2
-        GPIO.setup(self.Ena, GPIO.OUT) 
+        GPIO.setup(self.Ena, GPIO.OUT)
         GPIO.setup(self.In1, GPIO.OUT)
         GPIO.setup(self.In2, GPIO.OUT)
         self.pwm = GPIO.PWM(self.Ena, 100)
         self.pwm.start(0)
 
-    def moveF(self, s):
+    def moveF(self):
         GPIO.output(self.In1, GPIO.LOW)
         GPIO.output(self.In2, GPIO.HIGH)
-        self.pwm.ChangeDutyCycle(s)
 
-    def moveB(self, s):
+    def moveB(self):
         GPIO.output(self.In1, GPIO.HIGH)
         GPIO.output(self.In2, GPIO.LOW)
+
+    def stop(self):
+        GPIO.output(self.Ena, GPIO.LOW)
+
+    def drive(self, s):
         self.pwm.ChangeDutyCycle(s)
 
 
@@ -77,7 +84,10 @@ try:
     @sio.event
     def disconnect():
         print('disconnected from server')
-        
+        Motor1.stop()
+        Motor2.stop()
+        Motor3.stop()
+        Motor4.stop()
 
     @sio.on('drive-orders')
     def on_message(angle, speed):
@@ -90,8 +100,11 @@ try:
 
         # First Quadrant
         elif angle >= 0 and angle < 90:
-            motor1Speed = round(1023 * speed)
-            motor2Speed = round(remap(asMultiplier, 0, 90, 0, 2047))
+
+            motor1Speed = round(100 * speed)
+            Motor1.moveF()
+            motor2Speed = round(remap(asMultiplier, 0, 90, 0, 100))
+            Motor2.moveF()
 
         # Second Quadrant
         elif angle > 90 and angle <= 180:
@@ -108,15 +121,15 @@ try:
             motor1Speed = round(2047 * speed)
             motor2Speed = round(remap(asMultiplier, -180, -90, 0, 1023))
 
-        motor1.set_moving_speed(motor1Speed)
-        motor2.set_moving_speed(motor2Speed)
+        Motor1.drive(motor1Speed)
+        Motor2.drive(motor2Speed)
 
     sio.connect('http://192.168.2.12:3000')
     sio.wait()
 
 except KeyboardInterrupt:
     time.sleep(0.5)
-    motor1.set_moving_speed(0)
-    motor2.set_moving_speed(0)
-    motor1.disable_torque()
-    motor2.disable_torque()
+    Motor1.stop()
+    Motor2.stop()
+    Motor3.stop()
+    Motor4.stop()
